d7cb4eb417744aad3f8e80449eea5ea5f81ec8d2
Ram Singla <ram.singla@gmail.com>
2010-05-26 18:24:31 +0530

    Redirect to Home on New User Login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,18 @@
 class HomeController < ApplicationController
   
-  japi_connect_login_optional
+  japi_connect_login_optional :except => :index_with_login
   
   def index
-    @story_blocks = current_user.home_blocks( news_edition )
+    force_login = params.delete(:fl)
+    if force_login == '1' && current_user.new_record?
+      redirect_to :action => :index_with_login
+    else
+      @story_blocks = current_user.home_blocks( news_edition )
+    end
+  end
+  
+  def index_with_login
+    redirect_to :action => :index
   end
   
 end
