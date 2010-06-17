d862e062a9f83bc1fa644267899e2157e7b25dfe
Ram Singla <ram.singla@gmail.com>
2010-06-17 17:33:25 +0530

    Bugfix

--- app/models/page_data.rb
+++ app/models/page_data.rb
@@ -9,8 +9,7 @@ class PageData
   
   def initialize( user, options = {} )
     @multi_curb = Curl::Multi.new
-    @multi_curb.max_connects = 5
-    @multi_curb.pipeline = true
+    @multi_curb.pipeline = false
     @user = user
     @edition = ( options[:edition] || JAPI::PreferenceOption.parse_edition( user.edition || 'int-en' ) )
     set_user_preferences do |curb|
